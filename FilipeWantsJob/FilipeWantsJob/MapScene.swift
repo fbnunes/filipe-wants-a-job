51cf8a5f38f2b734ad3f58aee5a70d000a61967d
fbnunes <filipebnunes@gmail.com>
2022-09-09 22:19:00 +0200

    1.2 Testing nearest neighbor scaling
    It didn't work still, so possibly the best is to add the images bigger for now

--- FilipeWantsJob/FilipeWantsJob/MapScene.swift
+++ FilipeWantsJob/FilipeWantsJob/MapScene.swift
@@ -8,10 +8,19 @@
 import Foundation
 import SpriteKit
 
+
 class MapScene: SKScene {
     
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor.white
+        
+//        guard let uiImage = UIImage(named: "IMG_2832.PNG") else { return }
+//          let ciImage = CIImage(image: uiImage)
+        
+        let imageView = UIImageView(frame: CGRect(x: 150, y: 150, width: 1220, height: 1220))
+          imageView.image = UIImage(named: "IMG_2832.PNG")
+//        imageView.image?.ciImage?.samplingNearest()
+        self.view?.addSubview(imageView)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
