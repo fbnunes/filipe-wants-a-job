a926a219c28d092ba7d3afafe36418d6148e34bd
fbnunes <filipebnunes@gmail.com>
2022-09-18 18:48:51 +0200

    Sprite movement test

--- FilipeWantsJob/FilipeWantsJob/Scenes/GameScene.swift
+++ FilipeWantsJob/FilipeWantsJob/Scenes/GameScene.swift
@@ -10,8 +10,34 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+//    let playableRect: CGRect
+//
+//    var lastUpdateTime: TimeInterval = 0
+//    var dt: TimeInterval = 0
+//
+//    var lastTouchLocation: CGPoint?
+//
+//    let zombie = SKSpriteNode(imageNamed: "character")
+//    let zombieMovePointsPerSec: CGFloat = 480.0
+//    var velocity = CGPoint.zero
+//    let zombieRotateRadiansPerSec:CGFloat = 4.0 * π
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    
+//    override init(size: CGSize) {
+//      let maxAspectRatio:CGFloat = 16.0/9.0
+//      let playableHeight = size.width / maxAspectRatio
+//      let playableMargin = (size.height-playableHeight)/2.0
+//      
+//      playableRect = CGRect(x: 0, y: playableMargin,
+//                            width: size.width, height: playableHeight)
+//      super.init(size: size)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//      fatalError("init(coder:) has not been implemented")
+//    }
     
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor.white
@@ -88,7 +114,74 @@ class GameScene: SKScene {
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+//    override func update(_ currentTime: TimeInterval) {
+//        // Called before each frame is rendered
+//    }
+//    
+//    func moveZombieToward(_ location: CGPoint) {
+//      let offset = location - zombie.position
+//      let direction = offset.normalized()
+//      velocity = direction * zombieMovePointsPerSec
+//    }
+//    
+//    func rotateSprite(_ sprite: SKSpriteNode, direction: CGPoint, rotateRadiansPerSec: CGFloat) {
+//      let shortest = shortestAngleBetween(angle1: sprite.zRotation, angle2: velocity.angle)
+//      let amountToRotate = min(rotateRadiansPerSec * CGFloat(dt), abs(shortest))
+//      sprite.zRotation += shortest.sign() * amountToRotate
+//    }
+//    
+//    func moveSprite(_ sprite: SKSpriteNode, velocity: CGPoint) {
+//      let amountToMove = velocity * CGFloat(dt)
+//      print("Amount to move: \(amountToMove)")
+//      sprite.position += amountToMove
+//    }
+//    
+//    func boundsCheckZombie() {
+//      let bottomLeft = CGPoint(x: playableRect.minX, y: playableRect.minY)
+//      let topRight = CGPoint(x: playableRect.maxX, y: playableRect.maxY)
+//      
+//      if zombie.position.x <= bottomLeft.x {
+//        zombie.position.x = bottomLeft.x
+//        velocity.x = -velocity.x
+//      }
+//      if zombie.position.x >= topRight.x {
+//        zombie.position.x = topRight.x
+//        velocity.x = -velocity.x
+//      }
+//      if zombie.position.y <= bottomLeft.y {
+//        zombie.position.y = bottomLeft.y
+//        velocity.y = -velocity.y
+//      }
+//      if zombie.position.y >= topRight.y {
+//        zombie.position.y = topRight.y
+//        velocity.y = -velocity.y
+//      }
+//    }
+//    
+//    override func update(_ currentTime: TimeInterval) {
+//      if lastUpdateTime > 0 {
+//        dt = currentTime - lastUpdateTime
+//      } else {
+//        dt = 0
+//      }
+//      lastUpdateTime = currentTime
+//      print("\(dt*1000) milliseconds since last update")
+//      
+//      // moveSprite(zombie, velocity: CGPoint(x: zombieMovePointsPerSec, y: 0))
+//      if let lastTouchLocation = lastTouchLocation {
+//        let diff = lastTouchLocation - zombie.position
+//        if (diff.length() <= zombieMovePointsPerSec * CGFloat(dt)) {
+//          zombie.position = lastTouchLocation
+//          velocity = CGPoint.zero
+//        } else {
+//          moveSprite(zombie, velocity: velocity)
+//          rotateSprite(zombie, direction: velocity, rotateRadiansPerSec: zombieRotateRadiansPerSec)
+//        }
+//      }
+//      
+//      boundsCheckZombie()
+//    }
+//    
+    
 }
+
